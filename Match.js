0e9082499c3df7d2a62a90876cd0f6003904ab44
Markus <abramianmedina@gmail.com>
2023-10-23 18:30:16 +0100

    Tennis

---
+++ Match.js
@@ -0,0 +1,35 @@
+class Match{
+    players = [];
+    winner = null;
+    date = new Date();
+    
+    constructor(date){
+        if(date instanceof Date) {
+            this.date = date;
+        }else{
+            const [day, month, year] = date.split('/');
+            this.date = new Date([year, month, day].join('/'));
+        } 
+            
+    }
+    insertPlayer(player){
+        if (this.players.length<2) {
+            this.players.push(player);
+            return false;
+        }
+        this.players.push(player);
+        return true;
+    }
+
+    setWinner(player){
+        if (this.players.includes(player)) {
+            if (this.date < (new Date())) {
+                this.winner = player;
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+module.exports = Match;
